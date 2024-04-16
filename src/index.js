26c4af7fdff9eddb059886a8e8ea77abd6e62cc1
Hanuk <12fdamb@gmail.com>
2024-04-16 18:51:08 +0900

    0416

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './step01_jsx/Library';
+import Clock from './step02_element/Clock';
+import Welcome from './step03_component/Welcome';
+import Comment from './step03_component/Comment';
+import CommentList from './step03_component/CommentList';
+import NotificationList from './step04_state/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//UI 를 업데이트하는 유잉한 방법은 새로운 앨리먼트를 새성하고...
+// setInterval(()=>
+//   root.render(
+//     <React.StrictMode>
+//       {/* <App /> */}
+//       {/* <Library /> */}
+//       <Clock />
+//     </React.StrictMode>
+// ),1000);
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Library /> */}
+    {/* <Clock /> */}
+      {/* <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" /> */}
+      {/* <Comment /> */}
+      {/* <CommentList /> */}
+      <NotificationList />
   </React.StrictMode>
 );
 
---
+++ src/step03_component/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Welcome(props){
+    return(
+    <h1>Hello, {props.name}</h1>
+    )
+}
+
+export default Welcome;
+// 붕어빵 예제 1개의 틀로 3개를 만든다.
